47af830ab48fa91e9dbbae24f99b322a6980c39e
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-03-05 16:12:15 -0300

    Modify Di packages. Create Layouts

---
+++ app/src/main/java/com/example/apprickandmortycleanarch/di/HttpClient.kt
@@ -0,0 +1,12 @@
+package com.example.apprickandmortycleanarch.di
+
+import retrofit2.Retrofit
+
+class HttpClient(private val retrofit: Retrofit) {
+    fun <T> create(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+    inline fun <reified T : Any> create(): T {
+        return create(T::class.java)
+    }
+}
